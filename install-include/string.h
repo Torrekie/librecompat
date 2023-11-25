274d07fdcd0292cf7c92afee2c02347682522d35
Torrekie <31795847+Torrekie@users.noreply.github.com>
2023-11-25 15:57:47 +0800

    memrchr

--- install-include/string.h
+++ install-include/string.h
@@ -12,6 +12,7 @@ char *strchrnul(const char *s, int c);
 /* Torrekie: it seems compiler automatically optimizes mempcpy if provided declaration,
    but we are still providing implementation for mempcpy anyway. */
 void *mempcpy(void *__restrict dst, const void *__restrict src, size_t len);
+void *memrchr(const void *s, int c, size_t n);
 
 #ifdef __cplusplus
 }
