6a6df906af005469159b54a2e81d022e97c4f80d
Torrekie <31795847+Torrekie@users.noreply.github.com>
2026-01-07 17:42:18 +0800

    use a better redirecting approach for setlocale to avoid undefing (especially in m4 project)

--- install-include/locale.h
+++ install-include/locale.h
@@ -3,14 +3,11 @@
 
 #if defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__) && (__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 150000)
 
-#ifndef setlocale
-#define setlocale compat_setlocale
-#endif
-
 #include_next <locale.h>
 
 __BEGIN_DECLS
 
+char *setlocale(int, const char *) __asm("_compat_setlocale");
 char *compat_setlocale(int, const char *);
 
 __END_DECLS
